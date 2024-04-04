30635f306eda31790cc339f53249e037e40da92b
MERCHI-PC\Hyunwoo Lee <hyunwoo3785@gmail.com>
2024-04-04 17:26:52 +0900

    no message

---
+++ Source/RestAPI/Private/KakaoLocalAPI.cpp
@@ -0,0 +1,155 @@
+﻿#include "KakaoLocalAPI.h"
+#include "Online/HTTP/Public/Interfaces/IHttpRequest.h"
+#include "Online/HTTP/Public/HttpModule.h"
+#include "Online/HTTP/Public/Interfaces/IHttpResponse.h"
+#include "Components/BoxComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GenericPlatform/GenericPlatformHttp.h"
+
+
+AKakaoLocalAPIActor::AKakaoLocalAPIActor()
+{
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+	SceneComponent->SetupAttachment(RootComponent);
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	BoxCollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision Component"));
+	BoxCollisionComponent->SetupAttachment(SceneComponent);
+	
+
+	// Create a follow camera
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh Component"));
+	MeshComponent->SetupAttachment(SceneComponent);
+
+	
+
+	if (TObjectPtr<UStaticMesh> MeshObject = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), this->GetOuter(), TEXT("/Script/Engine.StaticMesh'/RestAPI/Meshes/SM_ChamferCube.SM_ChamferCube'"))))
+	{
+		MeshComponent->SetStaticMesh(MeshObject);
+	}
+	
+
+	if (TObjectPtr<UMaterialInterface> MaterialObject = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), this->GetOuter(), TEXT("/Script/Engine.MaterialInstanceConstant'/RestAPI/Materials/MI_Solid_Blue.MI_Solid_Blue'"))))
+	{
+		MeshComponent->SetMaterial(0, MaterialObject);
+	}
+	
+}
+
+
+void AKakaoLocalAPIActor::RequestSearchingAddress(const FString& Address)
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
+
+	Request->SetURL(FString::Printf(TEXT(ADDRESS_SEARCH_URL), *FGenericPlatformHttp::UrlEncode(Address)));
+	Request->SetVerb(TEXT("GET"));
+
+	Request->SetHeader(TEXT("Authorization"), FString(TEXT("KakaoAK ")) + KAKAO_REST_API_KEY);
+	Request->OnProcessRequestComplete().BindUObject(this, &AKakaoLocalAPIActor::OnSearchingAddressReceived);
+	Request->ProcessRequest();
+}
+
+void AKakaoLocalAPIActor::RequestSearchingKeyword(const FString& Keyword)
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
+
+	Request->SetURL(FString::Printf(TEXT(KEYWORD_SEARCH_URL), *FGenericPlatformHttp::UrlEncode(Keyword)));
+	Request->SetVerb(TEXT("GET"));
+
+	Request->SetHeader(TEXT("Authorization"), FString(TEXT("KakaoAK ")) + KAKAO_REST_API_KEY);
+	Request->OnProcessRequestComplete().BindUObject(this, &AKakaoLocalAPIActor::OnSearchingKeywordReceived);
+	Request->ProcessRequest();
+}
+
+void AKakaoLocalAPIActor::RequestSearchingLocation(const FString& InLatitude, const FString& InLongitude)
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
+
+	Request->SetURL(FString::Printf(TEXT(PLACES_SEARCH_URL), *InLatitude, *InLongitude));
+	Request->SetVerb(TEXT("GET"));
+
+	Request->SetHeader(TEXT("Authorization"), FString(TEXT("KakaoAK ")) + KAKAO_REST_API_KEY);
+	Request->OnProcessRequestComplete().BindUObject(this, &AKakaoLocalAPIActor::OnSearchingLocationReceived);
+	Request->ProcessRequest();
+}
+
+
+void AKakaoLocalAPIActor::OnSearchingAddressReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	ensureMsgf(false, TEXT("Not implemented"));
+}
+
+void AKakaoLocalAPIActor::OnSearchingKeywordReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	ensureMsgf(false, TEXT("Not implemented"));
+}
+
+void AKakaoLocalAPIActor::OnSearchingLocationReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	if (!bWasSuccessful)
+	{
+		ensureMsgf(false, TEXT("Searching location request failed."));
+	}
+
+	FString Result = Response->GetContentAsString();
+
+	if (Response->GetResponseCode() != 200)
+	{
+		ensureMsgf(false, TEXT("Responsed code : %d"), Response->GetResponseCode());
+	}
+	Places.Empty();
+	
+	TSharedPtr<FJsonObject> JsonObject;
+
+	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+
+	if (FJsonSerializer::Deserialize(Reader, JsonObject))
+	{
+		TArray<TSharedPtr<FJsonValue>> documents = JsonObject->GetArrayField("documents");
+
+		for (TSharedPtr<FJsonValue> Value : documents)
+		{
+			const TSharedPtr<FJsonObject>& Obj = Value->AsObject();
+			FKakaoPlace Place;
+			Place.AddressName			= Obj->GetStringField("adress_name");
+			Place.CategoryGroupCode		= Obj->GetStringField("category_group_code");
+			Place.CategoryGroupName		= Obj->GetStringField("category_group_name");
+			Place.CategoryName			= Obj->GetStringField("category_name");
+			Place.Distance				= Obj->GetStringField("distance");
+			Place.Id					= Obj->GetStringField("id");
+			Place.Latitude				= Obj->GetStringField("y");
+			Place.Longitude				= Obj->GetStringField("x");
+			Place.PhoneNumber			= Obj->GetStringField("phone");
+			Place.PlaceName				= Obj->GetStringField("place_name");
+			Place.PlaceUrl				= Obj->GetStringField("place_url");
+			Place.RoadAddressName		= Obj->GetStringField("road_address_name");
+
+			Places.Add(Place);
+		}
+		LocationSearchingPlacesDelegate.Broadcast(Places);
+	}
+}
+
+void AKakaoLocalAPIActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (BoxCollisionComponent)
+	{
+		BoxCollisionComponent->SetBoxExtent(FVector(BOX_COLLISION_EXTENT * CollsionBoxScale));
+		BoxCollisionComponent->SetRelativeLocation(FVector(0, 0, BOX_COLLISION_EXTENT * CollsionBoxScale));
+	}
+	if (MeshComponent)
+	{
+		MeshComponent->SetRelativeScale3D(FVector(1.f * CollsionBoxScale, 1.f * CollsionBoxScale, BOX_TO_FLAT_SCALER));
+		MeshComponent->SetRelativeLocation(FVector(0, 0, BOX_TO_FLAT_SCALER * 0.5f));
+	}
+}
+
+void AKakaoLocalAPIActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	RequestSearchingLocation(Latitude, Longitude);
+}
---
+++ Source/RestAPI/Public/KakaoLocalAPI.h
@@ -0,0 +1,101 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Online/HTTP/Public/Interfaces/IHttpRequest.h"
+#include "KakaoLocalAPI.generated.h"
+
+#define ADDRESS_SEARCH_URL "https://dapi.kakao.com/v2/local/search/address.json?query=%s"
+#define KEYWORD_SEARCH_URL "https://dapi.kakao.com/v2/local/search/keyword.json?query=%s"
+#define KEYWORD_SEARCH_URL_ADV "https://dapi.kakao.com/v2/local/search/keyword.json?query=%s&y=%s&x=%s&radius=%s" // keyword, y = latitude, x = longitude
+#define CATEGORY_SEARCH_URL "https://dapi.kakao.com/v2/local/search/category.json?category_group_code=%s"
+#define PLACES_SEARCH_URL "https://dapi.kakao.com/v2/local/search/category.json?category_group_code=%25%20&y=%s&x=%s&radius=50" //y = latitude, x = longitude, 검색반경은 임의로 50미터 설정
+
+#define KAKAO_REST_API_KEY "448edda50741aed5926540433c13d421" /*테스트 앱용 REST API 키*/
+
+
+USTRUCT(BlueprintType)
+struct RESTAPI_API FKakaoPlace
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString AddressName;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString RoadAddressName;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString CategoryGroupCode;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString CategoryGroupName;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString CategoryName;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString Distance;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString Id;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString PhoneNumber;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString PlaceName;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString PlaceUrl;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString Latitude;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place")
+	FString Longitude;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FLocationSearchingPlacesDelegate, const TArray<FKakaoPlace>&, Places);
+
+UCLASS(Abstract) 
+class RESTAPI_API AKakaoLocalAPIActor : public AActor
+{
+	GENERATED_BODY()
+public:	
+	AKakaoLocalAPIActor();
+
+	UFUNCTION(BlueprintCallable, Category = "KakaoAPI")
+	void RequestSearchingAddress(const FString& Address);
+	UFUNCTION(BlueprintCallable, Category = "KakaoAPI")
+	void RequestSearchingKeyword(const FString& Keyword);
+	UFUNCTION(BlueprintCallable, Category = "KakaoAPI")
+	void RequestSearchingLocation(const FString& Latitude, const FString& Longitude);
+
+	
+private:
+	void OnSearchingAddressReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	void OnSearchingKeywordReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	void OnSearchingLocationReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+private:
+	UPROPERTY(BlueprintAssignable, Category = "KakaoAPI")
+	FLocationSearchingPlacesDelegate LocationSearchingPlacesDelegate;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "KakaoAPI", meta = (AllowPrivateAccess = "true"))
+	class USceneComponent* SceneComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "KakaoAPI", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* BoxCollisionComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "KakaoAPI", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place", meta = (AllowPrivateAccess = "true"))
+	FString Latitude;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place", meta = (AllowPrivateAccess = "true"))
+	FString Longitude;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "KakaoAPI | Place", meta = (AllowPrivateAccess = "true"))
+	float CollsionBoxScale = 1.f;
+
+	UPROPERTY(VisibleAnywhere, Category = "KakaoAPI | Place")
+	TArray<FKakaoPlace> Places;
+
+	const int BOX_COLLISION_EXTENT = 50;
+	const float BOX_TO_FLAT_SCALER = 0.01f;
+public:
+	void OnConstruction(const FTransform& Transform) override;
+
+
+	void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+};
